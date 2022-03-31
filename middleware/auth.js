a67b18e0b82e735ac81f1aeb942889c1dd3daa32
Matthew McGrath <rubywep@hotmail.com>
2022-03-31 14:12:37 -0700

    Install jsonwebtoken & bcrypt. Start auth work.

---
+++ middleware/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+
+const auth = async (req, res, next) => {
+  try {
+    // Get the value of the token from the Authorization Header
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decoded = jwt.verify(token, 'thisisasecret');
+
+    // Our Token's payload stores our User ID
+    // So check if this Token is part of our User's Tokens field
+    const user = await User.findOne({
+      _id: decoded._id,
+      'tokens.token': token,
+    });
+
+    if (!user) {
+      throw new Error();
+    }
+
+    // No need for route handlers to fetch user again, as we set it here on the req, which routes can access!
+    req.user = user;
+    req.token = token;
+
+    // Make sure we pass control off to the next middleware
+    next();
+  } catch (error) {
+    res.status(401).send({ error: 'Please authenticate! ' });
+  }
+};
--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs/dist/bcrypt');
 const mongoose = require('mongoose');
 
 // Define the shape of a User model
@@ -7,10 +8,12 @@ const userSchema = mongoose.Schema(
     username: {
       type: String,
       required: [true, 'Please enter a user name'],
+      trim: true,
     },
     // The user's real name (for use in certain scenarios)
     name: {
       type: String,
+      default: 'Anonymous',
     },
     // A URL which serves as the user's profile picture
     imageURL: {
@@ -20,10 +23,77 @@ const userSchema = mongoose.Schema(
     favoriteStocks: {
       type: [String],
     },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
   },
   {
     timestamps: true,
   }
 );
+
+// This function is available on all instances of a User
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+
+  const token = jwt.sign({ _id: user._id.toString() }, 'thisisasecret');
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
+
+// This function is available on the Model itself
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  // No User with this email found?
+  if (!user) {
+    throw new Error('Unable to login');
+  }
+
+  const isMatch = await bcrupt.compare(password, user.password);
+
+  // Password incorrect?
+  if (!isMatch) {
+    throw new Error('Unable to login');
+  }
+
+  // User with email / password combination was successfully found!
+  return user;
+};
+
+// This method will be called right before a User model is saved
+// Hash the plain-text password before saving
+userSchema.pre('save', async function (next) {
+  // This is the individual User we are about to save
+  const user = this;
+
+  // No need to hash password if password hasn't even been modified (which will be the case upon user Registration or user editing their password)
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  // Signal that we are done and ready to pass control
+  next();
+});
+
+// Delete a User's Comments / Posts when the User is deleted?
+// For now, we will not use this -- as maybe we want to keep a history of a deleted User's Posts / Comments
+userSchema.pre('remove', async function (next) {
+  // This is the individual User we are about to delete
+  const user = this;
+
+  await Comment.deleteMany({ author: user._id });
+  await Post.deleteMany({ author: user._id });
+
+  next();
+});
 
 module.exports = mongoose.model('User', userSchema);
